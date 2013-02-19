479c5dc2d34b8c6419143cfd8fa49245f3d101d9
Roland Warmerdam <rowno@webspirited.com>
2013-02-19 20:05:25 +1300

    Add comments and keyword

--- tasks/mochacli.js
+++ tasks/mochacli.js
@@ -7,6 +7,8 @@ module.exports = function (grunt) {
     grunt.registerMultiTask('mochacli', 'Run Mocha server-side tests.', function () {
         var options = this.options();
         var globs = [];
+
+        // Use the Grunt files format if the `files` option isn't set
         if (!options.files) {
             this.files.forEach(function (glob) {
                 globs = globs.concat(glob.orig.src);
