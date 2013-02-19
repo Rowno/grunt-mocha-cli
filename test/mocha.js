c195e7034f20beacca1c40433539e5b67cc313ea
Roland Warmerdam <rowno@webspirited.com>
2013-02-19 20:55:59 +1300

    Normalise the Mocha output

--- test/mocha.js
+++ test/mocha.js
@@ -9,9 +9,9 @@ exports['sanity check'] = function (test) {
     mocha({
         files: [__dirname + '/fixture/pass.js'],
         quiet: true
-    }, function (error, result) {
+    }, function (error, output) {
         test.ifError(error);
-        test.notStrictEqual(result.stdout.indexOf('1 test complete'), -1, '1 test should run');
+        test.notStrictEqual(output.indexOf('1 test complete'), -1, '1 test should run');
         test.done();
     });
 };
@@ -23,10 +23,10 @@ exports['set string option'] = function (test) {
         files: [__dirname + '/fixture/pass.js'],
         quiet: true,
         reporter: 'json'
-    }, function (error, result) {
+    }, function (error, output) {
         test.ifError(error);
         test.doesNotThrow(function () {
-            JSON.parse(result.stdout);
+            JSON.parse(output);
         }, SyntaxError, 'should return valid JSON');
         test.done();
     });
@@ -40,24 +40,23 @@ exports['set bool option'] = function (test) {
         quiet: true,
         reporter: 'json',
         bail: true
-    }, function (error, result) {
-        var output = JSON.parse(result.stdout);
+    }, function (error, output) {
+        output = JSON.parse(output);
         test.strictEqual(output.stats.tests, 1, 'should run only 1 test');
         test.done();
     });
 };
 
 exports['set array option'] = function (test) {
-    test.expect(2);
+    test.expect(1);
 
     mocha({
         files: [__dirname + '/fixture/coffeescript.coffee'],
         quiet: true,
         reporter: 'json',
         compilers: ['coffee:coffee-script']
-    }, function (error, result) {
-        test.ifError(error);
-        test.equal(result.stderr, '', 'should compile the coffeescript');
+    }, function (error) {
+        test.ifError(error, 'should compile the coffeescript');
         test.done();
     });
 };
@@ -70,8 +69,8 @@ exports['require modules'] = function (test) {
         quiet: true,
         reporter: 'json',
         require: ['should']
-    }, function (error, result) {
-        var output = JSON.parse(result.stdout);
+    }, function (error, output) {
+        output = JSON.parse(output);
         test.ifError(error);
         test.strictEqual(output.stats.passes, 1, 'should require modules');
         test.done();
@@ -85,8 +84,8 @@ exports['file glob'] = function (test) {
         files: [__dirname + '/fixture/pass*.js'],
         quiet: true,
         reporter: 'json'
-    }, function (error, result) {
-        var output = JSON.parse(result.stdout);
+    }, function (error, output) {
+        output = JSON.parse(output);
         test.ifError(error);
         test.strictEqual(output.stats.suites, 2, 'should run 2 files');
         test.done();
