128ec22e6998cfb6b2c9d9ba2bbc273ff984ea1f
Roland Warmerdam <rowno@webspirited.com>
2013-12-06 23:16:28 +1300

    Rename the gruntfile fixtures

--- test/mochacli.js
+++ test/mochacli.js
@@ -8,7 +8,7 @@ exports['grunt pass'] = function (test) {
 
     grunt.util.spawn({
         grunt: true,
-        args: ['--gruntfile', __dirname + '/fixture/pass-gruntfile.js']
+        args: ['--gruntfile', __dirname + '/fixture/grunt-pass-gruntfile.js']
     }, function (error, output, code) {
         test.strictEqual(code, 0, 'grunt should pass');
         test.done();
@@ -20,19 +20,19 @@ exports['grunt fail'] = function (test) {
 
     grunt.util.spawn({
         grunt: true,
-        args: ['--gruntfile', __dirname + '/fixture/fail-gruntfile.js']
+        args: ['--gruntfile', __dirname + '/fixture/grunt-fail-gruntfile.js']
     }, function (error, output, code) {
         test.notStrictEqual(code, 0, 'grunt should fail');
         test.done();
     });
 };
 
-exports['grunt glob'] = function (test) {
+exports['grunt files'] = function (test) {
     test.expect(2);
 
     grunt.util.spawn({
         grunt: true,
-        args: ['--gruntfile', __dirname + '/fixture/glob-gruntfile.js']
+        args: ['--gruntfile', __dirname + '/fixture/grunt-files-gruntfile.js']
     }, function (error, output, code) {
         test.notStrictEqual(output.stdout.indexOf('2 passing'), -1, 'should pass 2 tests');
         test.strictEqual(code, 0, 'grunt should pass');
@@ -45,7 +45,7 @@ exports['grunt options'] = function (test) {
 
     grunt.util.spawn({
         grunt: true,
-        args: ['--gruntfile', __dirname + '/fixture/options-gruntfile.js']
+        args: ['--gruntfile', __dirname + '/fixture/grunt-options-gruntfile.js']
     }, function (error, output, code) {
         test.strictEqual(code, 0, 'grunt should pass');
         test.done();
@@ -57,7 +57,7 @@ exports['option force'] = function (test) {
 
     grunt.util.spawn({
         grunt: true,
-        args: ['--gruntfile', __dirname + '/fixture/force-gruntfile.js']
+        args: ['--gruntfile', __dirname + '/fixture/option-force-gruntfile.js']
     }, function (error, output, code) {
         test.strictEqual(code, 0, 'grunt should pass');
         test.done();
@@ -69,7 +69,7 @@ exports['option files'] = function (test) {
 
     grunt.util.spawn({
         grunt: true,
-        args: ['--gruntfile', __dirname + '/fixture/files-gruntfile.js']
+        args: ['--gruntfile', __dirname + '/fixture/option-files-gruntfile.js']
     }, function (error, output, code) {
         test.strictEqual(code, 0, 'grunt should pass');
         test.done();
